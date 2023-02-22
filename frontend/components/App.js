028350651f00045077c203a578552e8ca4e6f9b1
Joshua Kerr <kerrjoshua@gmail.com>
2023-02-22 21:59:50 +0900

    Still a mess. Working on fixing App and getting props out to components.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -18,22 +18,36 @@ export default function App() {
 
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+  const redirectToLogin = () => { navigate('/') }
+  const redirectToArticles = () => { navigate('articles') }
 
   const logout = () => {
     // ✨ implement
     // If a token is in local storage it should be removed,
+    if (localStorage.getItem('token')) localStorage.removeItem('token')
     // and a message saying "Goodbye!" should be set in its proper state.
+    setMessage('Goodbye!')
     // In any case, we should redirect the browser back to the login screen,
     // using the helper above.
+    redirectToLogin();
   }
 
   const login = ({ username, password }) => {
     // ✨ implement
     // We should flush the message state, turn on the spinner
     // and launch a request to the proper endpoint.
+    setMessage('');
+    setSpinnerOn(true);
     // On success, we should set the token to local storage in a 'token' key,
+    axios.post('http://localhost:9000/api/login', {username: username, password: password})
+      .then(res => {
+        setMessage(res.message);
+        localStorage.setItem('token', res.token);
+        navigate('articles')
+      })
+      .catch(err => setMessage(err.message))
+      .finally(setSpinnerOn(false));
+
     // put the server success message in its proper state, and redirect
     // to the Articles screen. Don't forget to turn off the spinner!
   }
@@ -42,6 +56,8 @@ export default function App() {
     // ✨ implement
     // We should flush the message state, turn on the spinner
     // and launch an authenticated request to the proper endpoint.
+    setMessage('');
+    setSpinnerOn(true);
     // On success, we should set the articles in their proper state and
     // put the server success message in its proper state.
     // If something goes wrong, check the status of the response:
@@ -68,8 +84,8 @@ export default function App() {
   return (
     // ✨ fix the JSX: `Spinner`, `Message`, `LoginForm`, `ArticleForm` and `Articles` expect props ❗
     <>
-      <Spinner />
-      <Message />
+      <Spinner on={spinnerOn}/>
+      <Message message={message}/>
       <button id="logout" onClick={logout}>Logout from app</button>
       <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
         <h1>Advanced Web Applications</h1>
@@ -78,11 +94,11 @@ export default function App() {
           <NavLink id="articlesScreen" to="/articles">Articles</NavLink>
         </nav>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={<LoginForm login={login}/>} />
           <Route path="articles" element={
             <>
-              <ArticleForm />
-              <Articles />
+              <ArticleForm postArticle={postArticle} updateArticle={updateArticle}/>
+              <Articles articles={articles} deleteArticle={deleteArticle} getArticles={getArticles}/>
             </>
           } />
         </Routes>
