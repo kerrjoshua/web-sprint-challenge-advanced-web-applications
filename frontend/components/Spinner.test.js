35cc887873941383ddc680914ff6709399422df3
Joshua Kerr <kerrjoshua@gmail.com>
2023-02-23 15:11:16 +0900

    Did student test.

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,15 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+import Spinner from './Spinner'
+import React from 'react'
+import { render, fireEvent, screen, waitForElementToBeRemoved } from '@testing-library/react'
+
+
+test('spinner renders without errors', () => {
+ render(<Spinner />)
+})
+
+test('Spinner shows message "Please wait." in appropriate relationship to the "on" property', () => {
+  const elem = screen.queryByText(/please wait/i)
+  expect(elem).toBeInTheDocument
 })
